abc357b99607e5e3c7bf73b0638656638a0f59f3
Corey Dow-Hygelund <cdowhygelund@mozilla.com>
2018-11-13 08:42:42 -0800

    high experiment.

---
+++ Energy_Consumption/experiments/Experiment - V0 - high.py
@@ -0,0 +1,34 @@
+import logging
+
+from os import path
+from Energy_Consumption.experiment import Experiment, Tasks, Task
+from helpers.io_helpers import log_to_stdout
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+log_to_stdout(logger, level=logging.DEBUG)
+
+exp_id = 5
+
+
+class TasksTest(Tasks):
+    @property
+    def tasks(self):
+        tasks = [
+            # wait for 2 minutes
+            Task('time.sleep(300)', self.client, meta={'website': 'HOME'}),
+            # go to Mozilla.org
+            Task("https://www.lingscars.com/')", self.client,
+                 meta={'website': 'https://www.lingscars.com/'}),
+            # wait for 2 minutes
+            Task('time.sleep(600)', self.client, meta={'website': 'HOME'}),
+        ]
+        return tasks
+
+
+""" DON'T MODIFY THE BELOW!!!!"""
+
+exp_name = path.splitext(path.basename(__file__))[0]
+exp = Experiment(exp_id=exp_id, exp_name=exp_name, tasks=TasksTest(), duration=1200)
+
+exp.run()
